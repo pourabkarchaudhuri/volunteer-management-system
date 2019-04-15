6041153c14a0b5b60b9168d646c0603722f9251b
PournimaM-Hexaware <44185@hexaware.com>
2019-04-15 10:31:28 +0530

    routes

--- src/handlers/admin_create_event.js
+++ src/handlers/admin_create_event.js
@@ -2,7 +2,7 @@ const { connection } = require('./vml_db')
 
 class Crud {
 
-    add_event_by_admin(event_name,activity,work,location,hexa_association,pre_vol_activities,testimonials,links){
+    add_event_by_admin(event_name,activity,work,location,hexa_association,pre_vol_activities,testimonials,links,callback){
         console.log("****inside add function****")
         console.log("event_name :",event_name)
         console.log("Activity :",activity)
@@ -11,56 +11,69 @@ class Crud {
         connection.connect().then(function () {
             connection.query("INSERT INTO event values('" + event_name + "','" + activity + "','" + work + "','" + location + "','" + hexa_association + "','" + pre_vol_activities + "','" + testimonials + "','" + links + "')").then(function (recordSet) {
                 console.log("****inserted successfully****")
+                callback(null,recordSet)
                 connection.close();
             }).catch(function (err) {
                 console.log("error while querying",err);
                 connection.close();
+                callback(err, null)
             });
         }).catch(function (err) {
             console.log("error while connection to db",err);
+            callback(err,null)
         });
     }
 
-    delete_event_by_admin(event_name) {
+    delete_event_by_admin(event_name, callback) {
         console.log("name of event", event_name)
         connection.connect().then(function () {
             connection.query("delete from event where event_name = '"+event_name+"'").then(function (recordSet) {
                 console.log("****successfully deleted****")
                 connection.close();
+                callback(null,recordSet)
             }).catch(function (err) {
                 console.log("error while querying",err);
                 connection.close();
+                callback(err,null)
             });
         }).catch(function (err) {
             console.log("error while connection to db",err);
+            callback(err,null)
         });
     }
 
-    update_event_by_admin(event_name,activity,work,location,hexa_association,pre_vol_activities,testimonials,links) {
+    update_event_by_admin(event_name,activity,work,location,hexa_association,pre_vol_activities,testimonials,links,callback) {
         connection.connect().then(function() {
             connection.query("update event set activity = '"+activity+"',set work = '"+work+"',set location = '"+location+"',set hexa_association = '"+hexa_association+"',set pre_vol_activities = '"+pre_vol_activities+"',set testimonials = '"+testimonials+"',set links = '"+links+"' where event_name = '"+event_name+"' ").then(function (recordSet) {
                 console.log("****Updated Successfully****")
                 connection.close();
+                callback(null,recordSet)
             }).catch(function (err) {
                 console.log("error while querying",err);
                 connection.close();
+                callback(err,null)
             });
         }).catch(function (err) {
             console.log("error while connection to db",err);
+            callback(err,null)
         });
     }
 
-    attended_list(emp_name, event_name, email) {
+    attended_list(emp_name, event_name, email, callback)
+    {
         connection.connect().then(function() {
             connection.query("insert into attendance values('"+ emp_name +"', '"+ event_name +"', '"+ email +"')").then(function (recordSet) {
-                console.log("****inserted into attendance table****");
+                console.log("****Updated Successfully****")
                 connection.close();
+                callback(null,recordSet)
             }).catch(function (err) {
                 console.log("error while querying",err);
                 connection.close();
+                callback(err,null)
             });
         }).catch(function (err) {
             console.log("error while connection to db",err);
+            callback(err,null)
         });
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,16 @@
 const express = require("express"),
-router = express.Router(),
-path = require('path'),
-fs = require('fs');
+    router = express.Router(),
+    path = require('path'),
+    fs = require('fs');
 var request = require("request");
 var pages = require('./handlers/pages');
 var authHelper = require('./handlers/authHelper');
 const { connection } = require('./handlers/vml_db')
 
-var dbcrud_admin =  require('./handlers/admin_create_event.js')
-var dbcrud_user =  require('./handlers/user_registration_event.js')
-var dboperation_admin =new dbcrud_admin()
-var dboperation_user =new dbcrud_user()
+var dbcrud_admin = require('./handlers/admin_create_event.js')
+var dbcrud_user = require('./handlers/user_registration_event.js')
+var dboperation_admin = new dbcrud_admin()
+var dboperation_user = new dbcrud_user()
 
 require('dotenv').config();
 //Import Environment Variables if any
@@ -18,39 +18,38 @@ require('dotenv').config();
 const viewPath = path.join(__dirname, '../views');
 //Configure paths
 
-router.get('/authorize', function(req, res) {
+router.get('/authorize', function (req, res) {
     console.log('Authorization Endpoint Hit!');
     var authCode = req.query.code;
     if (authCode) {
         console.log('Retrieved auth code in /authorize: ' + authCode);
         authHelper.getTokenFromCode(authCode, tokenReceived, req, res);
-    }else {
-      // redirect to home
+    } else {
+        // redirect to home
         console.log('/authorize called without a code parameter, redirecting to login');
         res.redirect('/');
     }
 });
 
-router.get('/logincomplete', function(req, res) {
+router.get('/logincomplete', function (req, res) {
     console.log('login completed successfully');
 
     loginCheck(req, res);
-    
+
     res.sendFile(viewPath + '/index.html');
 });
 
-router.get('/refreshtokens', function(req, res) {
+router.get('/refreshtokens', function (req, res) {
     var refresh_token = req.session.refresh_token;
     if (refresh_token === undefined) {
         console.log('no refresh token in session');
         res.redirect('/');
-    }
-    else {
+    } else {
         authHelper.getTokenFromRefreshToken(refresh_token, tokenReceived, req, res);
     }
 });
 
-router.get('/logout', function(req, res) {
+router.get('/logout', function (req, res) {
     console.log('Logout endpoint called!');
     req.session.destroy();
     res.redirect('/');
@@ -92,54 +91,103 @@ router.get("/create_events", function (req, res) {
 
 //on click add button by admin 
 router.post("/add_event", function (req, res) {
-    dboperation_admin.add_event_by_admin(req.body.event_name, req.body.activity, req.body.work, req.body.location, req.body.hexa_association, req.body.pre_vol_activities, req.body.testimonials, req.body.links)
-    res.sendFile(viewPath + '/events.html');
+    dboperation_admin.add_event_by_admin(req.body.event_name, req.body.activity, req.body.work, req.body.location, req.body.hexa_association, req.body.pre_vol_activities, req.body.testimonials, req.body.links, (err, data) => {
+        if (err) {
+            var output = {
+                error: {
+                    name: err.name,
+                    message: err.message,
+                    text: err.toString()
+                }
+            };
+            res.send(output)
+        } else {
+            res.send(data)
+        }
+    })
+    //res.sendFile(viewPath + '/events.html');
 });
 
 //on click button delete by admin
 router.delete("/delete_event", function (req, res) {
-    dboperation_admin.delete_event_by_admin(req.body.event_name)
-    res.sendFile(viewPath + '/events.html');
+    dboperation_admin.delete_event_by_admin(req.body.event_name, (err, data) => {
+        if (err) {
+            res.send('error',err)
+        } else {
+            res.send(data);
+        }
+    })
+    //res.sendFile(viewPath + '/events.html');
 });
 
 //on click edit button by admin
 router.put("/update_event", function (req, res) {
-    dboperation_admin.update_event_by_admin(req.body.event_name, req.body.activity, req.body.work, req.body.location, req.body.hexa_association, req.body.pre_vol_activities, req.body.testimonials, req.body.links)
-    res.sendFile(viewPath + '/events.html');
+    dboperation_admin.update_event_by_admin(req.body.event_name, req.body.activity, req.body.work, req.body.location, req.body.hexa_association, req.body.pre_vol_activities, req.body.testimonials, req.body.links, (err, data) => {
+        if (err) {
+            res.send('error',err)
+        } else {
+            res.send(data);
+        }
+    })
+    // res.sendFile(viewPath + '/events.html');
 });
 
 //on click add attended
 router.post("/attendedToEvent", function (req, res) {
-    dboperation_admin.attended_list(req.body.emp_name, req.body.event_name, req.body.email)
-    res.sendFile(viewPath + '/events.html');
+    dboperation_admin.attended_list(req.body.emp_name, req.body.event_name, req.body.email, (err, data) => {
+        if (err) {
+            res.send('error',err)
+        } else {
+            res.send(data);
+        }
+    })
+    // res.sendFile(viewPath + '/events.html');
 });
 
 //on click register button by user
 router.post("/register_event", function (req, res) {
-    dboperation_user.register_event(req.body.emp_name, req.body.emp_id, req.body.contact_no, req.body.email, req.body.event_name, req.body.event_time)
-    res.sendFile(viewPath + '/events.html');
+    dboperation_user.register_event(req.body.emp_name, req.body.emp_id, req.body.contact_no, req.body.email, req.body.event_name, req.body.event_time, (err, data) => {
+        if (err) {
+            res.send('error',err)
+        } else {
+            res.send(data);
+        }
+    })
+    // res.sendFile(viewPath + '/events.html');
 });
 
 //on click delete button by user, delete reg_event by id
 router.delete("/del_regi_event", function (req, res) {
-    dboperation_user.del_registration_by_user(req.body.event_name)
-    res.sendFile(viewPath + '/events.html');
+    dboperation_user.del_registration_by_user(req.body.event_name, (err, data) => {
+        if (err) {
+            res.send('error',err)
+        } else {
+            res.send(data);
+        }
+    })
+    // res.sendFile(viewPath + '/events.html');
 });
 
 //on click edit registration button by user update time
 router.put("/update_event_time", function (req, res) {
-    dboperation_user.editSlots_by_user(req.body.event_name, req.body.updated_time)
-    res.sendFile(viewPath + '/events.html');
+    dboperation_user.editSlots_by_user(req.body.event_name, req.body.updated_time, (err, data) => {
+        if (err) {
+            res.send('error',err)
+        } else {
+            res.send(data);
+        }
+    })
+    // res.sendFile(viewPath + '/events.html');
 });
 
 
 function tokenReceived(req, res, error, token) {
     if (error) {
-        console.log('ERROR getting token:'  + error);
+        console.log('ERROR getting token:' + error);
         res.send('ERROR getting token: ' + error);
     }
-else {
-    // save tokens in session
+    else {
+        // save tokens in session
         console.log('Incoming Token : ' + JSON.stringify(token))
         req.session.access_token = token.token.access_token;
         req.session.refresh_token = token.token.refresh_token;
@@ -148,27 +196,27 @@ else {
     }
 }
 
-function loginCheck(req, res){
+function loginCheck(req, res) {
     console.log('Session Data : ' + JSON.stringify(req.session.email));
     var temp = req.session.email;
     console.log(temp)
     connection.connect().then(function () {
         connection.query("SELECT * FROM admin WHERE email = '" + temp + "'").then(function (recordSet) {
             console.log(recordSet);
-            console.log("recordSet.recordsets :",recordSet.recordsets)
+            console.log("recordSet.recordsets :", recordSet.recordsets)
             var temp2 = recordSet.recordset.length;
-            console.log("temp :",temp2)
-			if (temp2 > 0) {
-				console.log("admin")
-			} else {
-				console.log("user")
+            console.log("temp :", temp2)
+            if (temp2 > 0) {
+                console.log("admin")
+            } else {
+                console.log("user")
             }
             connection.close();
         }).catch(function (err) {
             console.log(err);
             connection.close();
         });
-	})
+    })
     if (req.session.access_token === undefined || req.session.refresh_token === undefined) {
         console.log('/ called while not logged in');
         res.redirect('/');
